ee2bdc88408aa057611b0655d4fe43a334aa9f3d
MatthewP089 <47394866+MatthewP089@users.noreply.github.com>
2019-05-14 16:25:00 +0100

    Added TaskAllocate

--- src/taskAllocation/Task.java
+++ src/taskAllocation/Task.java
@@ -10,9 +10,25 @@ public class Task {
 	private int taskUrgency;
 	private boolean taskSuspended;
 	private String assignedTo;
+	private String dueDate;
+	private String assignedDate;
+	
+	/*
+	 * constructer for a task within the system 
+	 * @param taskname the name of the task
+	 * @param jobNumber the number of the job the task is within 
+	 * @param taskNotes short description of the task 
+	 * @param taskDuration the expected duration of the task 
+	 * @param taskComplete if the task is completed 
+	 * @param taskUrgency a number representing how urgent the task is
+	 * @param taskSuspended if the task is suspended or not
+	 * @param assignedTo a user name of a technician in which the task is assigned to 
+	 * @param dueDate the exact date and time in which the task is due by 
+	 * @param assignedDate the exact date and time in which the date was assigned 
+	 */
 	
 	public Task(String taskName, int jobNumber, String taskNotes, int taskDuration, boolean taskComplete,
-			int taskUrgency, boolean taskSuspended, String assignedTo) {
+			int taskUrgency, boolean taskSuspended, String assignedTo, String dueDate,String assignedDate) {
 		this.taskName = taskName;
 		this.jobNumber = jobNumber;
 		this.taskNotes = taskNotes;
@@ -21,12 +37,23 @@ public class Task {
 		this.taskUrgency = taskUrgency;
 		this.taskSuspended = taskSuspended;
 		this.assignedTo = assignedTo;
+		this.dueDate = dueDate;
+		this.assignedDate = assignedDate;
 		
 	}
 
+
+	/*
+	 * constructer for a task within the system 
+	 * @param taskname the name of the task
+	 * @param jobNumber the number of the job the task is within 
+	 * @param taskNotes short description of the task 
+	 * @param taskDuration the expected duration of the task 
+	 * @param taskComplete if the task is completed 
+	 * @param assignedTo a user name of a technician in which the task is assigned to 
+	 */
 	public Task(String taskName, int jobNumber, String taskNotes, int taskDuration, boolean taskComplete,
 			String assignedTo) {
-		super();
 		this.taskName = taskName;
 		this.jobNumber = jobNumber;
 		this.taskNotes = taskNotes;
@@ -36,72 +63,185 @@ public class Task {
 	}
 
 
-
+	/*
+ 	* get the name of the task 
+ 	* 
+ 	* return taskName 
+ 	*/
 	public String getTaskName() {
 		return taskName;
 	}
 
+	/*
+ 	* set the name of the task 
+ 	* 
+ 	* @param taskName the name of the task 
+ 	*/
 	public void setTaskName(String taskName) {
 		this.taskName = taskName;
 	}
 
+	/*
+ 	* get the job number the task is assigned to
+ 	* 
+ 	* return jobNumber  
+ 	*/
 	public int getJobNumber() {
 		return jobNumber;
 	}
 
+	/*
+ 	* set the job number the task is assigned to
+ 	* 
+ 	* @param jobNumber the job number the task is assigned to  
+ 	*/
 	public void setJobNumber(int jobNumber) {
 		this.jobNumber = jobNumber;
 	}
 
+	/*
+ 	* get the short description of the task 
+ 	* 
+ 	* return taskNotes 
+ 	*/
 	public String getTaskNotes() {
 		return taskNotes;
 	}
 
+	/*
+ 	* set the short description of the task 
+ 	* 
+ 	* @param taskNotes a short description of the task 
+ 	*/
 	public void setTaskNotes(String taskNotes) {
 		this.taskNotes = taskNotes;
 	}
 
+	/*
+ 	* get the expected duration
+ 	* 
+ 	* return taskDuration
+ 	*/
 	public int getTaskDuration() {
 		return taskDuration;
 	}
 
+	/*
+ 	* set the expected duration
+ 	* 
+ 	* @param taskDuration the expected duration of the task 
+ 	*/
 	public void setTaskDuration(int taskDuration) {
 		this.taskDuration = taskDuration;
 	}
 
+	/*
+ 	* get true or false if the task is complete 
+ 	* 
+ 	* return taskComplete
+ 	*/
 	public boolean isTaskComplete() {
 		return taskComplete;
 	}
 
+	/*
+ 	* set true or false if the task is complete 
+ 	* 
+ 	* @param taskComplete true or false if the task is complete 
+ 	*/
 	public void setTaskComplete(boolean taskComplete) {
 		this.taskComplete = taskComplete;
 	}
 
+	/*
+ 	* get the urgency of the task 
+ 	* 
+ 	* return taskUrgency
+ 	*/
 	public int getTaskUrgency() {
 		return taskUrgency;
 	}
 
+	/*
+ 	* set the name of the task 
+ 	* 
+ 	* @param taskUrgency the urgency of the task 
+ 	*/
 	public void setTaskUrgency(int taskUrgency) {
 		this.taskUrgency = taskUrgency;
 	}
 
+	/*
+ 	* get true or false if the task is suspended or not 
+ 	* 
+ 	* return taskSuspended
+ 	*/
 	public boolean isTaskSuspended() {
 		return taskSuspended;
 	}
 
+	/*
+ 	* set true or false if the task is suspended or not 
+ 	* 
+ 	* @param taskSuspended true or false if the task is suspended
+ 	*/
 	public void setTaskSuspended(boolean taskSuspended) {
 		this.taskSuspended = taskSuspended;
 	}
 
+	/*
+ 	* get the name of the technician in which the task is assigned to 
+ 	* 
+ 	* return assignedTo  
+ 	*/
 	public String getAssignedTo() {
 		return assignedTo;
 	}
 
+	/*
+ 	* set the name of the technician in which the task is assigned to 
+ 	* 
+ 	* @param assignedTo name of the technician being assigned the task  
+ 	*/
 	public void setAssignedTo(String assignedTo) {
 		this.assignedTo = assignedTo;
 	}
 	
+	/*
+ 	* get the exact date and time in which the task was assigned 
+ 	* 
+ 	* return assignedDate 
+ 	*/
+	public String getAssignedDate() {
+		return assignedDate;
+	}
+
+	/*
+ 	* set the exact date and time in which the task was assigned 
+ 	* 
+ 	* @param assignedDate the exact date and time in which the task was assigned
+ 	*/
+	public void setAssignedDate(String assignedDate) {
+		this.assignedDate = assignedDate;
+	}
 	
+	/*
+ 	* get the exact date and time in which the task is due 
+ 	* 
+ 	* return dueDate
+ 	*/
+	public String getDueDate() {
+		return dueDate;
+	}
+
+	/*
+ 	* set the exact date and time in which the task is due 
+ 	* 
+ 	* @param dueDate the exact date and time in which the task is due 
+ 	*/
+	public void setDueDate(String dueDate) {
+		this.dueDate = dueDate;
+	}
 	
 
 }
